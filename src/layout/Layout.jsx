d17a476a7ba6e3ced436e726f001ece2c2dc85ed
mdhsaikats <saikatsikder2911@gmail.com>
2026-08-17 03:55:57 +0600

    changes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import NavBar from '../components/Navbar';
+import NavBar from '../components/NavBar';
 import ShareIdeaModal from '../components/ShareIdeaModal';
 
 const Layout = () => {
